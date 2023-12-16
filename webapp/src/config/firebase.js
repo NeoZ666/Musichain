8057c1db11e90768de8c56ad278648c2d082f8b6
Nilanchal <nilanchalpanda2003@gmail.com>
2023-12-16 13:59:53 +0530

    Created React App and Integrated Firebase Database

---
+++ webapp/src/config/firebase.js
@@ -0,0 +1,15 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyC0_DSFWuIS7uJGFSMIGvIlCJr1tvjWees",
+  authDomain: "musichain-eafa2.firebaseapp.com",
+  projectId: "musichain-eafa2",
+  storageBucket: "musichain-eafa2.appspot.com",
+  messagingSenderId: "1049514648194",
+  appId: "1:1049514648194:web:e69f4701a61accb6754a9b",
+  measurementId: "G-3C5VDCW8VT"
+};
+
+
+const app = initializeApp(firebaseConfig);
