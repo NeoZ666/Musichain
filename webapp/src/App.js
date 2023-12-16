46824b5e3b96dc160b0dfbbfddea09f0e409e84d
Nilanchal <nilanchalpanda2003@gmail.com>
2023-12-16 15:24:39 +0530

    Added tailwindCSS and signup form

--- webapp/src/App.js
+++ webapp/src/App.js
@@ -1,9 +1,11 @@
 import React from 'react'
+import UserSignUp from './pages/UserSingup'
 
 function App() {
   return (
     <div> 
-      <h1>HELLO WORLD</h1>
+      <h1 className='text-3xl font-bold bg-red-500'>HELLO WORLD</h1>
+      <UserSignUp />
     </div>
   )
 }
