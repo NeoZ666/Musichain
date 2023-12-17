79f19be0a3a215b6ec86601c651fa86f85e3b540
Harsh <115883668+NeoZ666@users.noreply.github.com>
2023-12-17 10:03:58 +0530

    Update App.js

--- solidity/src/App.js
+++ solidity/src/App.js
@@ -1,88 +1,75 @@
-import React, { useState } from "react";
-import Web3 from "web3";
-import MusicLicensingContract from "./MusicLicensingContract.json";
-import AppForm from "./AppForm";
+import React, { useState, useEffect } from 'react';
+import Web3 from 'web3';
+import MusicLicensingContract from './MusicLicensingContract.json';
+import AppForm from './AppForm';
+
+const contractAddress = '0x0F8f0C4B2e66f3C2b682CA1BCdF35d9d0f324729';
+const contractABI = MusicLicensingContract.abi;
 
 const App = () => {
   const [web3, setWeb3] = useState(null);
   const [contract, setContract] = useState(null);
   const [account, setAccount] = useState(null);
+  const [transactionResult, setTransactionResult] = useState(null);
 
-  const initWeb3 = async () => {
-    if (window.ethereum) {
-      const web3 = new Web3(window.ethereum);
-      setWeb3(web3);
+  useEffect(() => {
+    const initWeb3 = async () => {
+      if (window.ethereum) {
+        const web3Instance = new Web3(window.ethereum);
+        setWeb3(web3Instance);
 
-      try {
-        await window.ethereum.enable();
-        const accounts = await web3.eth.getAccounts();
-        setAccount(accounts[0]);
+        try {
+          await window.ethereum.enable();
+          const accounts = await web3Instance.eth.getAccounts();
+          setAccount(accounts[0]);
 
-        const networkId = await web3.eth.net.getId();
-        const contractInstance = new web3.eth.Contract(
-          MusicLicensingContract.abi,
-          "0xB1BDDD23843fc5832F060842a54d6AbD86EE09D2"
-        );
-        setContract(contractInstance);
-      } catch (error) {
-        console.error("Error initializing web3", error);
+          const contractInstance = new web3Instance.eth.Contract(contractABI, contractAddress);
+          setContract(contractInstance);
+        } catch (error) {
+          console.error('Error initializing web3', error);
+        }
+      } else {
+        console.error('Please install MetaMask to use this application');
       }
-    } else {
-      console.error("Please install MetaMask to use this application");
-    }
-  };
+    };
+
+    initWeb3();
+  }, []);
 
   const handleFormSubmit = async (formData) => {
     try {
-      // Check if MetaMask is connected
-      if (!contract || !web3 || !account) {
-        console.error("Contract or web3 or account not initialized");
+      if (!web3 || !account) {
+        console.error('MetaMask not connected');
         return;
       }
 
-      // Convert fee to wei
-      const feeInEther = formData.fee;
-      const feeInWei = web3.utils.toWei(feeInEther.toString(), "ether");
+      const licenseFeeInEther = formData.licenseFee;
+      const licenseFeeInWei = web3.utils.toWei(licenseFeeInEther.toString(), 'ether');
 
-      // Estimate gas
-      const gas = await contract.methods
-        .completeLicenseTransaction()
-        .estimateGas({
-          from: account,
-          value: feeInWei,
-        });
-
-      // Send transaction
-      const result = await contract.methods.completeLicenseTransaction().send({
+      await contract.methods.payLicenseFee().send({
         from: account,
-        value: feeInWei,
-        gas,
+        value: licenseFeeInWei,
+        gas: 300000,
       });
 
-      console.log("Transaction result:", result);
+      setTransactionResult('Transaction successful');
     } catch (error) {
-      console.error("Error submitting form:", error);
+      console.error('Error submitting form:', error);
+      setTransactionResult('Transaction failed');
     }
   };
 
   return (
-    // <div>
-    //   <h1>Music Licensing App</h1>
-    //   <div>
-    //     <p>Connected Account: {account}</p>
-    //     <AppForm onSubmit={handleFormSubmit} />
-    //   </div>
-    //   <p>Loading Web3...</p>
-    // </div>
-
     <div>
-      <h1>Music Licensing App</h1>
-      <div>
-        <p>Connected Account: {account}</p>
-        <button onClick={initWeb3}>Connect MetaMask</button>
-        <AppForm onSubmit={handleFormSubmit} />
-      </div>
-      <p>Loading Web3...</p>
+      <h1>Music Licensing Web App</h1>
+      <p>Connected Account: {account}</p>
+      <AppForm onSubmit={handleFormSubmit} />
+      {transactionResult && (
+        <div>
+          <h3>Transaction Result:</h3>
+          <p>{transactionResult}</p>
+        </div>
+      )}
     </div>
   );
 };
