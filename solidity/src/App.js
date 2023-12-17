307e576fe69985ccbc6d14d368339d2a636fab32
Harsh <115883668+NeoZ666@users.noreply.github.com>
2023-12-17 05:33:54 +0530

    hello there~....I am sleepy

---
+++ solidity/src/App.js
@@ -0,0 +1,81 @@
+import React, { useState } from 'react';
+import Web3 from 'web3';
+import MusicLicensingContract from './MusicLicensingContract.json';
+import AppForm from './AppForm';
+
+const App = () => {
+  const [web3, setWeb3] = useState(null);
+  const [contract, setContract] = useState(null);
+  const [account, setAccount] = useState(null);
+
+  const initWeb3 = async () => {
+    if (window.ethereum) {
+      const web3 = new Web3(window.ethereum);
+      setWeb3(web3);
+
+      try {
+        await window.ethereum.enable();
+        const accounts = await web3.eth.getAccounts();
+        setAccount(accounts[0]);
+
+        const networkId = await web3.eth.net.getId();
+        const contractInstance = new web3.eth.Contract(
+          MusicLicensingContract.abi,
+          '0xB1BDDD23843fc5832F060842a54d6AbD86EE09D2'
+        );
+        setContract(contractInstance);
+      } catch (error) {
+        console.error('Error initializing web3', error);
+      }
+    } else {
+      console.error('Please install MetaMask to use this application');
+    }
+  };
+
+  const handleFormSubmit = async (formData) => {
+    try {
+      // Check if MetaMask is connected
+      if (!web3 || !account) {
+        console.error('MetaMask not connected');
+        return;
+      }
+
+      // Convert fee to wei
+      const feeInEther = formData.fee;
+      const feeInWei = web3.utils.toWei(feeInEther.toString(), 'ether');
+
+      // Estimate gas
+      const gas = await contract.methods.completeLicenseTransaction().estimateGas({
+        from: account,
+        value: feeInWei,
+      });
+
+      // Send transaction
+      const result = await contract.methods.completeLicenseTransaction().send({
+        from: account,
+        value: feeInWei,
+        gas,
+      });
+
+      console.log('Transaction result:', result);
+    } catch (error) {
+      console.error('Error submitting form:', error);
+    }
+  };
+
+  return (
+    <div>
+      <h1>Music Licensing App</h1>
+      {web3 && account ? (
+        <div>
+          <p>Connected Account: {account}</p>
+          <AppForm onSubmit={handleFormSubmit} />
+        </div>
+      ) : (
+        <p>Loading Web3...</p>
+      )}
+    </div>
+  );
+};
+
+export default App;
