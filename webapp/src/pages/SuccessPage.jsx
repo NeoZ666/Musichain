4a16bed478108c8086fe9f7ddc14ba46a470cbf0
Nilanchal <nilanchalpanda2003@gmail.com>
2024-01-21 22:41:18 +0530

    Minor changes.

--- webapp/src/pages/SuccessPage.jsx
+++ webapp/src/pages/SuccessPage.jsx
@@ -1,32 +1,3 @@
-// import React from "react";
-
-// const SuccessPage = () => {
-//   return (
-//     <div className="flex justify-center items-center min-h-screen">
-//       <img src="./images/CONGRATS.png" alt="CONGRATS MESSAGE" />
-
-//       <div
-//         className="flex gap-x-10 items-center break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter"
-//         onClick={() => {
-//           navigator.clipboard.writeText();
-//         }}
-//       >
-//         <h1>LINK</h1>
-//         <div>
-//           <img
-//             src="./images/copy.svg"
-//             alt="COPY"
-//             width={20}
-//             className="cursor-pointer"
-//           />
-//         </div>
-//       </div>
-//     </div>
-//   );
-// };
-
-// export default SuccessPage;
-
 import React from "react";
 
 const SuccessPage = () => {
@@ -45,23 +16,28 @@ const SuccessPage = () => {
       });
   };
 
-  const cid = "https://bafybeieqk54xq4iofv4q6wumppve53mipdbptuqmws4ufgn4dz2cd7k3a4.ipfs.nftstorage.link/"
+  const cid = "";
 
   return (
     <div className="flex justify-center items-center min-h-screen">
       <img src="./images/CONGRATS.png" alt="CONGRATS MESSAGE" />
-      <div className="text-white text-2xl font-bold mt-10">Song in now licensed to you</div>
 
-      <div className="flex gap-x-10 items-center break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter">
-        <h1>{cid}</h1>
-        <div>
-          <img
-            src="./images/copy.svg"
-            alt="COPY"
-            width={20}
-            className="cursor-pointer"
-            onClick={copyText}
-          />
+      <div className="flex flex-col gap-y-5">
+        <div className="text-white text-2xl font-bold mt-10">
+          Here's your Licensed Song
+        </div>
+
+        <div className="flex gap-x-10 items-center break-inside-avoid rounded-lg border border-gray-300 bg-white/20 bg-clip-padding p-6 pb-4 backdrop-blur-lg backdrop-filter">
+          <h1>{cid}</h1>
+          <div>
+            <img
+              src="./images/copy.svg"
+              alt="COPY"
+              width={20}
+              className="cursor-pointer"
+              onClick={copyText}
+            />
+          </div>
         </div>
       </div>
     </div>
